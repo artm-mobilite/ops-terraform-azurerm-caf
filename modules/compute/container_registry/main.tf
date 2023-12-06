aba092836190398d7e64a3e748b2b86d5be05600
Alexandre Chatelain <alexandre@chatelain.io>
2023-12-06 11:45:05 -0500

    fix managed identities

--- modules/compute/container_registry/main.tf
+++ modules/compute/container_registry/main.tf
@@ -17,13 +17,13 @@ locals {
   resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
 
   managed_local_identities = flatten([
-    for managed_identity_key in try(var.identity.managed_identity_keys, []) : [
+    for managed_identity_key in try(var.settings.identity.managed_identity_keys, []) : [
       var.managed_identities[var.client_config.landingzone_key][managed_identity_key].id
     ]
   ])
 
   managed_remote_identities = flatten([
-    for lz_key, value in try(var.identity.remote, []) : [
+    for lz_key, value in try(var.settings.identity.remote, []) : [
       for managed_identity_key in value.managed_identity_keys : [
         var.managed_identities[lz_key][managed_identity_key].id
       ]
