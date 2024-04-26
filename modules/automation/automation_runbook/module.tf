931043a0613890c97306549cb9c919cb25060cd3
Olivier Comeau <ocomeau@artm.quebec>
2024-04-26 18:32:17 +0000

    fix(automation-runbook): Adjust path to file

--- modules/automation/automation_runbook/module.tf
+++ modules/automation/automation_runbook/module.tf
@@ -12,7 +12,7 @@
 
 data "local_file" "automation_runbook_file" {
   count    = try(var.settings.content, null) == null && try(var.settings.content_filename, null) != null ? 1 : 0
-  filename = "${path.root}/${var.settings.content_filename}"
+  filename = "${path.cwd}/${var.settings.content_filename}"
 }
 
 resource "azurerm_automation_runbook" "automation_runbook" {
