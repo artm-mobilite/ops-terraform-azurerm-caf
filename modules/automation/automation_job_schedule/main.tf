1a57ac7a419d18007aec041ce35a9730bea5b135
Olivier Comeau <ocomeau@artm.quebec>
2024-04-29 22:54:07 +0000

    fix(automation): Remove local rg

--- modules/automation/automation_job_schedule/main.tf
+++ modules/automation/automation_job_schedule/main.tf
@@ -5,7 +5,3 @@ terraform {
     }
   }
 }
-
-locals {
-  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
-}
--- modules/automation/automation_job_schedule/module.tf
+++ modules/automation/automation_job_schedule/module.tf
@@ -1,5 +1,5 @@
 resource "azurerm_automation_job_schedule" "automation_job_schedule" {
-  resource_group_name     = local.resource_group_name
+  resource_group_name     = var.resource_group_name
   automation_account_name = var.automation_account_name
   schedule_name           = var.automation_account_schedule_name
   runbook_name            = var.automation_account_runbook_name
